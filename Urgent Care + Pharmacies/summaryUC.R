03bb3714d4f3999fc980c76e238b7141cb112af4
katerabinowitz <cathryn.rabinowitz@gmail.com>
2016-09-26 18:55:22 -0400

    uc graph

--- Urgent Care + Pharmacies/summaryUC.R
+++ Urgent Care + Pharmacies/summaryUC.R
@@ -11,8 +11,16 @@ pharm<-read.csv("/Users/katerabinowitz/Documents/DataLensDC/FOIA-Requests/Pharma
 
 uc<-read.csv("/Users/katerabinowitz/Documents/DataLensDC/DC-Health/Urgent Care + Pharmacies/urgentCare.csv",
              fill = FALSE, strip.white = TRUE,stringsAsFactors=FALSE)
-uc<-filter(uc,type!="hospital")
-table(uc$WARD_ID) 
+#focus is on private sector urgent/retail clinics so removing hospitals and community health centers
+uc<-filter(uc,type!="hospital" & !(grepl("Health",uc$type)))
+
+ucSum<-plyr::count(uc,c("WARD_ID"))
+ward8UC<-data.frame(8,0)
+ward4UC<-data.frame(4,0)
+colnames(ward8UC)<-c("WARD_ID","freq")
+colnames(ward4UC)<-c("WARD_ID","freq")
+ucSum<-rbind(ucSum,ward4UC,ward8UC)
+ucSum<-arrange(ucSum,WARD_ID)
 
 pharmVac = readOGR("http://opendata.dc.gov/datasets/2335ba275c3f4320a3113f13181eab56_9.geojson",
                    "OGRGeoJSON")
@@ -28,15 +36,9 @@ noPharm<-c("American University Student Health Center","Arthritis and Rheumatism
 
 pharmVac<-filter(pharmVac,!(NAME %in% noPharm | ADDRESS %in% noPharm))
 
-pvSum<-cbind(plyr::count(pharm,"WARD_ID"),plyr::count(pharmVac,"WARD_ID"))
+ucpSum<-cbind(plyr::count(pharm,"WARD_ID"),plyr::count(pharmVac,"WARD_ID"),ucSum)[c(1,2,4,6)]
 
-ucSum<-plyr::count(uc,"WARD_ID")
-ward8UC<-data.frame(8,0)
-colnames(ward8UC)<-c("WARD_ID","freq")
-ucSum<-rbind(ucSum,ward8UC)
-
-ucpSum<-cbind(pvSum,ucSum)[c(1,2,4,6)]
-colnames(ucpSum)<-c("ward","pharmacy","pharmacyVaccine","urgentCare")
+colnames(ucpSum)<-c("ward","pharmacy","pharmacyVaccine","ucrClinic")
 
 ucpSum<-mutate(ucpSum,ward=factor(ward))
 
@@ -61,6 +63,7 @@ ggplot(data = ucpSum, aes(x = ward, y = pharmacyVaccine)) +
        title="Vaccine options vary dramatically by ward",
        subtitle="Number of pharmacies administering vaccines by ward",
        caption="\nSource: DC Open Data, DataLensDC")
+ggsave("/Users/katerabinowitz/Documents/DataLensDC/Website/DataLensDCsite/Images/pharmVac.png")
 
 ggplot(data = ucpSum, aes(x = ward, y = pharmacy)) + 
   geom_bar(stat="identity", colour="#38c7c7",fill="#38c7c7") + 
@@ -80,3 +83,30 @@ ggplot(data = ucpSum, aes(x = ward, y = pharmacy)) +
        title="Fewer Pharmacies East of the Anacostia",
        subtitle="Number of pharmacies by ward",
        caption="\nSource: DC Department of Health, DataLensDC")
+ggsave("/Users/katerabinowitz/Documents/DataLensDC/Website/DataLensDCsite/Images/pharm.png")
+
+ggplot(data = ucpSum, aes(x = ward, y = ucrClinic)) + 
+  geom_bar(stat="identity", colour="#38c7c7",fill="#38c7c7") +
+  scale_y_continuous(breaks=seq(0, 10, 2),expand=c(0,0)) +
+  scale_x_discrete(expand=c(0,0)) +
+  theme(legend.position="bottom") + 
+  theme(legend.title=element_blank()) +
+  theme(legend.text=element_text(color="#505050")) +
+  theme(legend.background = element_rect(fill = '#EFF0F1')) +
+  theme(legend.key = element_rect(color = '#EFF0F1')) +
+  theme(legend.text=element_text(color="#505050")) +  
+  theme(axis.text.x = element_text()) +
+  theme(axis.text.y = element_text()) +
+  theme(plot.background = element_rect(fill = '#EFF0F1'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(text = element_text(size=13)) +
+  theme(axis.title.x = element_text(color="#505050")) +
+  theme(axis.title.y = element_text(color="#505050")) +
+  theme(plot.title=element_text(family="Helvetica", size=15, face="bold")) +
+  theme(plot.subtitle=element_text(color="#505050")) +
+  theme(plot.caption=element_text(color="#808080")) +
+  labs(x="Ward", y="Urgent care and retail clinics", 
+       title="Uneven Growth of Urgent Care and Retail Clinics in DC",
+       subtitle="Number of urgent care and retail clinics by ward",
+       caption="\nSource: DataLensDC, urgentcarelocations.com")
+ggsave("/Users/katerabinowitz/Documents/DataLensDC/Website/DataLensDCsite/Images/urgentRetailCare.png")
